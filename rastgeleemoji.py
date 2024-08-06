0e34a00cca201ae97e15e19a5ea4763e959b3429
EmirEfeDuyar <emirefeduyar9@gmail.com>
2024-08-06 14:43:43 +0300

    Create rastgeleemoji.py

---
+++ rastgeleemoji.py
@@ -0,0 +1,10 @@
+my_words = ["Hi!", "Python Pro", "Kodland"] 
+print(my_words[1])
+
+# name = input("Lütfen isminizi giriniz: ")
+# print("Hi ", name)
+
+import random
+emojis = ["^^", "0_o", ":)", "¯\\_(ツ)_/¯", "(￢_￢)"]
+rastgele_emoji = random.choice(emojis)
+print(rastgele_emoji)
